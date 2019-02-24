e559a181dc61b99de4f394c15135245c379998ed
Kyle_Tecson <kyltecs@gmail.com>
2019-02-23 22:49:13 -0800

    test of resuplying change/stock and checking current change/stock

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -29,4 +29,79 @@ describe("Vending Machine", () => {
       expect(result).toEqual({ name: "Candy", change: 0 });
     });
   });
+
+  describe("purchase an item with more change than needed", () => {
+    test("returns product name and change for item", () => {
+      const result = item.buyItem("c4", 1.0);
+      expect(result).toEqual({
+        name: "Potato",
+        change: ["QUARTERS", 6]
+      });
+    });
+  });
+
+  describe("purchase item with incorrect currency", () => {
+    test("returns the currency given", () => {
+      const result = item.buyItem(" a1", "pens");
+      expect(result).toEqual("pens");
+    });
+  });
+
+  describe("checking vending machine coins", () => {
+    test("returns list of current coins", () => {
+      const result = item.checkCurrentChange();
+      expect(result).toEqual([
+        ["QUARTERS", 33.5],
+        ["LOONIES", 40.0],
+        ["TOONIES", 50.0]
+      ]);
+    });
+  });
+
+  describe("resuply coins in vending machine", () => {
+    test("returns list of resuplied coins", () => {
+      const result = item.resuplyChange();
+      expect(result).toEqual([
+        ["QUARTERS", 50.0],
+        ["LOONIES", 60.0],
+        ["TOONIES", 100.0]
+      ]);
+    });
+  });
+
+  describe("check current stock of items in vending machine", () => {
+    test("returns list of items and thier inventory", () => {
+      const result = item.checkCurrentStock();
+      expect(result).toEqual([
+        "Chips: 10",
+        "Candy: 9",
+        "Pop: 10",
+        "Water: 0",
+        "Fruit: 10",
+        "Coffee: 10",
+        "Cookie: 10",
+        "Energy Drink: 10",
+        "Instant Noodles: 10",
+        "Potato: 9"
+      ]);
+    });
+  });
+
+  describe("restock items in vending machine", () => {
+    test("return list of new stock of items", () => {
+      const result = item.resuplyStock();
+      expect(result).toEqual([
+        "Chips: 10",
+        "Candy: 15",
+        "Pop: 10",
+        "Water: 10",
+        "Fruit: 10",
+        "Coffee: 20",
+        "Cookie: 30",
+        "Energy Drink: 10",
+        "Instant Noodles: 10",
+        "Potato: 14"
+      ]);
+    });
+  });
 });
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,6 +1,6 @@
 class VendingMachine {
-  constructor(inventory) {
-    this.data = require(inventory);
+  constructor(allItem) {
+    this.data = require(allItem);
     this.stock = this.data.stock;
     this.change = this.data.change;
   }
@@ -19,7 +19,7 @@ class VendingMachine {
       return acc;
     }, {});
 
-    const coins = VALUES((acc, currency) => {
+    const coins = VALUES.reduce((acc, currency) => {
       let amount = 0;
 
       while (currency.val <= amountOwed && amountOwed !== 0) {
@@ -34,6 +34,11 @@ class VendingMachine {
 
       return acc;
     }, []);
+
+    change.forEach(value => {
+      value[1] = vendingChange[value[0]];
+    });
+    return coins;
   }
 
   buyItem(item, cost) {
@@ -44,6 +49,8 @@ class VendingMachine {
         name: this.stock[item].name,
         change: this.stock[item].price
       };
+    } else if (typeof cost !== "number" || cost > 10 || cost < 0.05) {
+      return cost;
     } else if (this.stock[item].price - cost > 0) {
       const excess = this.stock[item].price - cost;
       return {
@@ -56,6 +63,36 @@ class VendingMachine {
       name: this.stock[item].name,
       change: this.calChange(this.change.current, this.stock[item].price, cost)
     };
+  }
+
+  checkCurrentChange() {
+    return this.change.current;
+  }
+
+  resuplyChange() {
+    this.change.max.forEach((change, index) => {
+      this.change.current[index] = change;
+    });
+    return this.change.current;
+  }
+
+  checkCurrentStock() {
+    let inv = [];
+    const indexArr = Object.keys(this.stock);
+    indexArr.forEach(stock => {
+      inv.push(`${this.stock[stock].name}: ${this.stock[stock].inventory}`);
+    });
+    return inv;
+  }
+
+  resuplyStock() {
+    let inv = [];
+    const indexArr = Object.keys(this.stock);
+    indexArr.forEach(stock => {
+      this.stock[stock].inventory = this.stock[stock].maxInv;
+      inv.push(`${this.stock[stock].name}: ${this.stock[stock].inventory}`);
+    });
+    return inv;
   }
 }
 
